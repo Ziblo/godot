61cc9e37132a4eaab39f0c9fd0e4469025a29592
Ziblo <Maximillian.Sayre@gmail.com>
2025-01-31 15:53:24 -0800

    started adding populate_surface function to multimesh_instance_3d

--- scene/3d/multimesh_instance_3d.cpp
+++ scene/3d/multimesh_instance_3d.cpp
@@ -53,7 +53,184 @@ void MultiMeshInstance3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_multimesh", "multimesh"), &MultiMeshInstance3D::set_multimesh);
 	ClassDB::bind_method(D_METHOD("get_multimesh"), &MultiMeshInstance3D::get_multimesh);
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "multimesh", PROPERTY_HINT_RESOURCE_TYPE, "MultiMesh"), "set_multimesh", "get_multimesh");
+    
+	ClassDB::bind_method(D_METHOD("populate_surface", "target_surface", "source_mesh", 
+                                  "mesh_up_axis", "random_rotation", "random_tilt", 
+                                  "random_scale", "scale", "amount"), 
+                         &MultiMeshInstance3D::populate_surface);
 }
+
+
+void MultiMeshInstance3D::populate_surface(Ref<Mesh> target_surface, Ref<Mesh> source_mesh, 
+                                                       UP_AXIS mesh_up_axis, float random_rotation, 
+                                                       float random_tilt, float random_scale, 
+                                                       float scale, int amount) {
+	Ref<Mesh> mesh;
+
+	surface_source->set_text("..");
+	mesh_source->set_text("..");
+	populate_axis->select(1);
+	populate_rotate_random->set_value(0);
+	populate_tilt_random->set_value(0);
+	populate_scale_random->set_value(0);
+	populate_scale->set_value(1);
+	populate_amount->set_value(128);
+
+	if (mesh_source->get_text().is_empty()) {
+		if (multimesh.is_null()) {
+			ERR_FAIL_MSG("No mesh source specified (and no MultiMesh set in node).");
+			return;
+		}
+		if (multimesh->get_mesh().is_null()) {
+			ERR_FAIL_MSG("No mesh source specified (and MultiMesh contains no Mesh).");
+			return;
+		}
+
+		mesh = multimesh->get_mesh();
+	} else {
+		Node *ms_node = get_node(mesh_source->get_text());
+
+		if (!ms_node) {
+			ERR_FAIL_MSG("Mesh source is invalid (invalid path).");
+			return;
+		}
+
+		MeshInstance3D *ms_instance = Object::cast_to<MeshInstance3D>(ms_node);
+
+		if (!ms_instance) {
+			ERR_FAIL_MSG("Mesh source is invalid (not a MeshInstance3D).");
+			return;
+		}
+
+		mesh = ms_instance->get_mesh();
+
+		if (mesh.is_null()) {
+			ERR_FAIL_MSG("Mesh source is invalid (contains no Mesh resource).");
+			return;
+		}
+	}
+
+	if (surface_source->get_text().is_empty()) {
+		ERR_FAIL_MSG("No surface source specified.");
+		return;
+	}
+
+	Node *ss_node = node->get_node(surface_source->get_text());
+
+	if (!ss_node) {
+		ERR_FAIL_MSG("Surface source is invalid (invalid path).");
+		return;
+	}
+
+	MeshInstance3D *ss_instance = Object::cast_to<MeshInstance3D>(ss_node);
+
+	if (!ss_instance || ss_instance->get_mesh().is_null()) {
+		ERR_FAIL_MSG("Surface source is invalid (no geometry).");
+		return;
+	}
+
+	Transform3D geom_xform = node->get_global_transform().affine_inverse() * ss_instance->get_global_transform();
+
+	Vector<Face3> geometry = ss_instance->get_mesh()->get_faces();
+
+	if (geometry.size() == 0) {
+		ERR_FAIL_MSG("Surface source is invalid (no faces).");
+		return;
+	}
+
+	//make all faces local
+
+	int gc = geometry.size();
+	Face3 *w = geometry.ptrw();
+
+	for (int i = 0; i < gc; i++) {
+		for (int j = 0; j < 3; j++) {
+			w[i].vertex[j] = geom_xform.xform(w[i].vertex[j]);
+		}
+	}
+
+	Vector<Face3> faces = geometry;
+	int facecount = faces.size();
+	ERR_FAIL_COND_MSG(!facecount, "Parent has no solid faces to populate.");
+
+	const Face3 *r = faces.ptr();
+
+	float area_accum = 0;
+	RBMap<float, int> triangle_area_map;
+	for (int i = 0; i < facecount; i++) {
+		float area = r[i].get_area();
+		if (area < CMP_EPSILON) {
+			continue;
+		}
+		triangle_area_map[area_accum] = i;
+		area_accum += area;
+	}
+
+	ERR_FAIL_COND_MSG(triangle_area_map.is_empty(), "Couldn't map area.");
+	ERR_FAIL_COND_MSG(area_accum == 0, "Couldn't map area.");
+
+	Ref<MultiMesh> multimesh = memnew(MultiMesh);
+	multimesh->set_mesh(mesh);
+
+	int instance_count = populate_amount->get_value();
+
+	multimesh->set_transform_format(MultiMesh::TRANSFORM_3D);
+	multimesh->set_use_colors(false);
+	multimesh->set_instance_count(instance_count);
+
+	float _tilt_random = populate_tilt_random->get_value();
+	float _rotate_random = populate_rotate_random->get_value();
+	float _scale_random = populate_scale_random->get_value();
+	float _scale = populate_scale->get_value();
+	int axis = populate_axis->get_selected();
+
+	Transform3D axis_xform;
+	if (axis == Vector3::AXIS_Z) {
+		axis_xform.rotate(Vector3(1, 0, 0), -Math_PI * 0.5);
+	}
+	if (axis == Vector3::AXIS_X) {
+		axis_xform.rotate(Vector3(0, 0, 1), -Math_PI * 0.5);
+	}
+
+	for (int i = 0; i < instance_count; i++) {
+		float areapos = Math::random(0.0f, area_accum);
+
+		RBMap<float, int>::Iterator E = triangle_area_map.find_closest(areapos);
+		ERR_FAIL_COND(!E);
+		int index = E->value;
+		ERR_FAIL_INDEX(index, facecount);
+
+		// ok FINALLY get face
+		Face3 face = r[index];
+		//now compute some position inside the face...
+
+		Vector3 pos = face.get_random_point_inside();
+		Vector3 normal = face.get_plane().normal;
+		Vector3 op_axis = (face.vertex[0] - face.vertex[1]).normalized();
+
+		Transform3D xform;
+
+		xform.set_look_at(pos, pos + op_axis, normal);
+		xform = xform * axis_xform;
+
+		Basis post_xform;
+
+		post_xform.rotate(xform.basis.get_column(1), -Math::random(-_rotate_random, _rotate_random) * Math_PI);
+		post_xform.rotate(xform.basis.get_column(2), -Math::random(-_tilt_random, _tilt_random) * Math_PI);
+		post_xform.rotate(xform.basis.get_column(0), -Math::random(-_tilt_random, _tilt_random) * Math_PI);
+
+		xform.basis = post_xform * xform.basis;
+		//xform.basis.orthonormalize();
+
+		xform.basis.scale(Vector3(1, 1, 1) * (_scale + Math::random(-_scale_random, _scale_random)));
+
+		multimesh->set_instance_transform(i, xform);
+	}
+
+	node->set_multimesh(multimesh);
+}
+
+
 
 void MultiMeshInstance3D::_notification(int p_what) {
 	if (p_what == NOTIFICATION_ENTER_TREE) {
